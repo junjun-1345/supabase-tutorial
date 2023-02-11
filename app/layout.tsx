3d8f9f51f4614532563c55a655143b8f40e6707f
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2023-02-11 18:05:36 +0900

    🎉初めてのコミット

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styles/globals.css";
+import Navigation from "./components/navigation";
+
+//レイアウト
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html>
+      <body>
+        <div className="flex flex-col min-h-screen">
+          <Navigation />
+
+          <main className="flex-1 container max-w-screen-xl mx-auto px-5 py-10">
+            {children}
+          </main>
+
+          <footer className="py-5 border-t">
+            <div className="text-center text-sm text-gray-500">
+              Copyright © All rights reserved | FullStackChannel
+            </div>
+          </footer>
+        </div>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
